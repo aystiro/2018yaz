8afe9370af53cd966279a14af5eef0452825f2a0
aystiro <emiryilmaz98@gmail.com>
2018-07-24 16:36:04 +0300

    Sparse matrix that's diagonal is 1

---
+++ sparsematrix.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class sparsematrix_1_0 {
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter the size of sparse matrix. (Ex: For 9x9 table, enter 9.)");
+		int size = keyboard.nextInt();
+		int row = size+1, column = size+1;
+		int [][] sparsematrix = new int [row][column];
+	
+		
+		for(int x = 0; x < row; x++) 
+		{
+			for(int y = 0; y < column; y++)
+			{
+				if(x == y)
+					sparsematrix[x][y] = 1;
+				else
+					sparsematrix[x][y] = 0;
+			}
+		}
+		
+		for(int x = 0; x < row; x++) 
+		{
+			System.out.println("");
+			for(int y = 0; y < column; y++)
+			{
+				System.out.print(sparsematrix[x][y] + " ");
+			}
+		}	
+	}
+	
+}
